30a8ae27fdb0c68b5a3bc1d7e56abcdbd289c835
JaViKo500 <javikogutierrez64@gmail.com>
2025-03-03 10:54:47 -0500

    doc(exceptions): add comment in identification exceptions

--- lib/exceptions/identification_exception.dart
+++ lib/exceptions/identification_exception.dart
@@ -1,9 +1,14 @@
 import 'package:ec_validations/entities/index.dart';
 
+/// Exception thrown during identification validation processes.
 class IdentificationException implements Exception {
   final String message;
   final ErrorCode code;
 
+  /// Creates a new identification validation exception.
+  ///
+  /// [code] The error code for this exception.
+  /// [message] The error message for this exception.
   IdentificationException(this.code, this.message);
 
   @override
