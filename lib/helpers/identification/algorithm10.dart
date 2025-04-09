05058f5803eefbad00359910a43db848aa5201fe
JaViKo500 <javikogutierrez64@gmail.com>
2025-04-08 22:38:54 -0500

    fix(validation): check valid verification digit if 0

--- lib/helpers/identification/algorithm10.dart
+++ lib/helpers/identification/algorithm10.dart
@@ -43,8 +43,11 @@ bool algorithm10(String firstDigits, String verificationDigitString) {
 
   final residue = total % 10;
 
-  final verificationDigitResult = 10 - residue;
-
+  int  verificationDigitResult = 0;
+  if ( residue != 0 ) {
+    verificationDigitResult = 10 - residue;
+  }
+  
   if (verificationDigitResult != verificationDigit) {
     throw IdentificationException(
       ErrorCode.invalidVerificationDigit,
