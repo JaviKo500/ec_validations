62047e9f8e38074e0e686728e51d8431e009ed51
JaViKo500 <javikogutierrez64@gmail.com>
2026-09-10 23:55:16 -0500

    feat(entities): add normalizedNumber to PhoneResult

--- lib/entities/phone.dart
+++ lib/entities/phone.dart
@@ -8,11 +8,16 @@ enum PhoneType {
 /// result of phone number validation
 class PhoneResult {
   final bool isValid;
+  /// normalized phone number if valid, otherwise null
+  final String? normalizedNumber;
+  /// error message if invalid, otherwise null
   final String? errorMessage;
+  /// error code if invalid, otherwise null
   final PhoneErrorCode? typeCodeError;
 
   PhoneResult({
     required this.isValid,
+    this.normalizedNumber,
     this.errorMessage,
     this.typeCodeError,
   });
