96d9c7f0cc89d478e8b426ba75fc8a1880d7ab3e
JaViKo500 <javikogutierrez64@gmail.com>
2026-09-10 23:56:02 -0500

    feat(validators): add isValidInternational and normalizedNumber

--- lib/validators/phone_validator.dart
+++ lib/validators/phone_validator.dart
@@ -12,9 +12,10 @@ class PhoneValidator {
   /// Returns a [PhoneResult] with the validation result.
   static PhoneResult isValidLocal( String phoneNumber ) {
     try {
-      validateLocalPhone( phoneNumber );
+      final validatedNumber = validateLocalPhone( phoneNumber );
       return PhoneResult(
         isValid: true,
+        normalizedNumber: validatedNumber,
         errorMessage: null,
         typeCodeError: null,
       );
@@ -27,6 +28,36 @@ class PhoneValidator {
         );
       }
 
+      return PhoneResult(
+        isValid: false,
+        errorMessage: 'Invalid phone number',
+        typeCodeError: PhoneErrorCode.invalidPhone,
+      );
+    }
+  }
+
+  /// Validates if an international Ecuadorian phone number is valid.
+  ///
+  /// [phoneNumber] Phone number to validate.
+  /// 
+  /// Returns a [PhoneResult] with the validation result.
+  static PhoneResult isValidInternational( String phoneNumber ) {
+    try {
+      final validatedNumber = validateInternationalPhone( phoneNumber );
+      return PhoneResult(
+        isValid: true,
+        normalizedNumber: validatedNumber,
+        errorMessage: null,
+        typeCodeError: null,
+      );
+    } catch (e) {
+      if ( e is PhoneException ) {
+        return PhoneResult(
+          isValid: false,
+          errorMessage: e.message,
+          typeCodeError: e.code,
+        );
+      }
       return PhoneResult(
         isValid: false,
         errorMessage: 'Invalid phone number',
