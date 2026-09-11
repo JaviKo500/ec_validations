24903dbc4ff563a1458e257f28ae48865a2aa92c
JaViKo500 <javikogutierrez64@gmail.com>
2026-09-10 23:08:20 -0500

    feat(validators): add PhoneValidator

--- lib/ec_validations.dart
+++ lib/ec_validations.dart
@@ -1,2 +1,3 @@
 export './validators/dni_validator.dart';
 export './validators/ruc_validator.dart';
+export './validators/phone_validator.dart';
---
+++ lib/validators/phone_validator.dart
@@ -0,0 +1,37 @@
+import 'package:ec_validations/entities/index.dart';
+import 'package:ec_validations/exceptions/index.dart';
+import 'package:ec_validations/helpers/index.dart';
+
+/// Validator for Ecuadorian phone numbers.
+class PhoneValidator {
+
+  /// Validates if a local Ecuadorian phone number is valid.
+  /// 
+  /// [phoneNumber] Phone number to validate.
+  ///   
+  /// Returns a [PhoneResult] with the validation result.
+  static PhoneResult isValidLocal( String phoneNumber ) {
+    try {
+      validateLocalPhone( phoneNumber );
+      return PhoneResult(
+        isValid: true,
+        errorMessage: null,
+        typeCodeError: null,
+      );
+    } catch (e) {
+      if ( e is PhoneException ) {
+        return PhoneResult(
+          isValid: false,
+          errorMessage: e.message,
+          typeCodeError: e.code,
+        );
+      }
+
+      return PhoneResult(
+        isValid: false,
+        errorMessage: 'Invalid phone number',
+        typeCodeError: PhoneErrorCode.invalidPhone,
+      );
+    }
+  }
+}
